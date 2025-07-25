4028c68b3bb9a287712e6cd06bb12054b482a320
ascadalebout <cadalebout1@cougars.ccis.edu>
2025-07-25 18:06:36 -0500

    changed harpoon keymaps to tab instead of alt

--- init.lua
+++ init.lua
@@ -150,12 +150,6 @@ vim.o.splitbelow = true
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Tab Configuration
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
 
@@ -958,6 +952,80 @@ require('lazy').setup({
   },
 
   {
+    'aliqyan-21/darkvoid.nvim',
+    config = function()
+      require('darkvoid').setup {
+        transparent = false,
+        glow = true,
+        show_end_of_buffer = true,
+
+        colors = {
+          fg = '#c0c0c0',
+          bg = '#1c1c1c',
+          cursor = '#bdfe58',
+          line_nr = '#404040',
+          visual = '#303030',
+          comment = '#585858',
+          string = '#d1d1d1',
+          func = '#e1e1e1',
+          kw = '#f1f1f1',
+          identifier = '#b1b1b1',
+          type = '#a1a1a1',
+          -- type_builtin = '#c5c5c5', -- current
+          type_builtin = '#8cf8f7', -- glowy blue old (was present by default before type_builtin was introduced added here for people who may like it)
+          search_highlight = '#1bfd9c',
+          operator = '#1bfd9c',
+          bracket = '#e6e6e6',
+          preprocessor = '#4b8902',
+          bool = '#66b2b2',
+          constant = '#b2d8d8',
+
+          -- enable or disable specific plugin highlights
+          plugins = {
+            gitsigns = true,
+            nvim_cmp = true,
+            treesitter = true,
+            nvimtree = true,
+            telescope = true,
+            lualine = true,
+            bufferline = true,
+            oil = true,
+            whichkey = true,
+            nvim_notify = true,
+          },
+
+          -- gitsigns colors
+          added = '#baffc9',
+          changed = '#ffffba',
+          removed = '#ffb3ba',
+
+          -- Pmenu colors
+          pmenu_bg = '#1c1c1c',
+          pmenu_sel_bg = '#1bfd9c',
+          pmenu_fg = '#c0c0c0',
+
+          -- EndOfBuffer color
+          eob = '#3c3c3c',
+
+          -- Telescope specific colors
+          border = '#585858',
+          title = '#bdfe58',
+
+          -- bufferline specific colors
+          bufferline_selection = '#1bfd9c',
+
+          -- LSP diagnostics colors
+          error = '#dea6a0',
+          warning = '#d6efd8',
+          hint = '#bedc74',
+          info = '#7fa1c3',
+        },
+      }
+      -- vim.cmd.colorscheme 'darkvoid'
+    end,
+  },
+
+  {
     'rebelot/kanagawa.nvim',
     config = function()
       require('kanagawa').setup {
@@ -968,7 +1036,7 @@ require('lazy').setup({
         keywordStyle = { italic = true },
         statementStyle = { bold = true },
         typeStyle = {},
-        transparent = false, -- do not set background color
+        transparent = false, -- set background to clear
         dimInactive = false, -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
         colors = { -- add/modify theme and palette colors
@@ -1062,14 +1130,14 @@ require('lazy').setup({
   --
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
+
   require 'kickstart.plugins.indent_line',
+  require 'custom.plugins.harpoon',
+  -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-  require 'custom.plugins.harpoon',
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
