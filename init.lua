c775ac359cef74a6b082b0acdb7a2599b080b4ca
Carl Dalebout <cadalebout1@cougars.ccis.edu>
2025-07-27 13:07:01 -0500

    created ssh key for github

--- init.lua
+++ init.lua
@@ -187,8 +187,8 @@ vim.keymap.set('n', '<leader>fe', '<cmd>Ex<CR>', { desc = 'Opens [F]ile [E]xplor
 
 vim.keymap.set('n', 'H', '^', { desc = 'move to first char in the row' })
 vim.keymap.set('n', 'L', '$', { desc = 'move to last char in the row' })
-vim.keymap.set('n', 'J', '}')
-vim.keymap.set('n', 'K', '{')
+vim.keymap.set({ 'n', 'v' }, 'J', '}')
+vim.keymap.set({ 'n', 'v' }, 'K', '{')
 
 -- TIP: Disable arrow keys
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
@@ -979,7 +979,6 @@ require('lazy').setup({
           preprocessor = '#4b8902',
           bool = '#66b2b2',
           constant = '#b2d8d8',
-
           -- enable or disable specific plugin highlights
           plugins = {
             gitsigns = true,
@@ -1026,6 +1025,39 @@ require('lazy').setup({
   },
 
   {
+    'rmehri01/onenord.nvim',
+    config = function()
+      require('onenord').setup {
+        theme = 'dark', -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+        borders = true, -- Split window borders
+        fade_nc = false, -- Fade non-current windows, making them more distinguishable
+        -- Style that is applied to various groups: see `highlight-args` for options
+        styles = {
+          comments = 'NONE',
+          strings = 'NONE',
+          keywords = 'NONE',
+          functions = 'NONE',
+          variables = 'NONE',
+          diagnostics = 'underline',
+        },
+        disable = {
+          background = false, -- Disable setting the background color
+          float_background = false, -- Disable setting the background color for floating windows
+          cursorline = false, -- Disable the cursorline
+          eob_lines = true, -- Hide the end-of-buffer lines
+        },
+        -- Inverse highlight for different groups
+        inverse = {
+          match_paren = false,
+        },
+        custom_highlights = {}, -- Overwrite default highlight groups
+        custom_colors = {}, -- Overwrite default colors
+      }
+      vim.cmd.colorscheme 'onenord.nvim'
+    end,
+  },
+
+  {
     'rebelot/kanagawa.nvim',
     config = function()
       require('kanagawa').setup {
@@ -1049,7 +1081,7 @@ require('lazy').setup({
           light = 'lotus',
         },
       }
-      vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
     end,
     build = function()
       vim.cmd 'KanagawaCompile'
